2e0c2c6a39f2a541d7d8f3d48febe9f978772bee
TheGod7 <br.iancefi12@gmail.com>
2025-12-23 21:01:56 -0600

    chore(app): configure global environment variables with ConfigModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: ['.env', '../.env'],
+    }),
+  ],
 })
 export class AppModule {}

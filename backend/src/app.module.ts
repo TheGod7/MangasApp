2c821b4cce2eeb2b1cb5761c07df1c91d5427641
TheGod7 <br.iancefi12@gmail.com>
2025-12-24 00:32:35 -0600

    chore(app): import MediaModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './infra/database/database.module';
+import { MediaModule } from './infra/media/media.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { DatabaseModule } from './infra/database/database.module';
       envFilePath: ['.env', '../.env'],
     }),
     DatabaseModule,
+    MediaModule,
   ],
 })
 export class AppModule {}

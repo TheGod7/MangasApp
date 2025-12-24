9f397fbd2dc9476efccf5d47d6bbf8cecc110a8f
TheGod7 <br.iancefi12@gmail.com>
2025-12-24 16:16:49 -0600

    chore(media): integrate MulterModule for async media configuration

--- backend/src/infra/media/media.module.ts
+++ backend/src/infra/media/media.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { mediaConfig } from './media.config';
 
 @Module({
-  imports: [CloudinaryModule],
+  imports: [
+    CloudinaryModule,
+    MulterModule.registerAsync(mediaConfig.asProvider()),
+  ],
 })
 export class MediaModule {}

116c6bea6b3d140bcc258abd8d44d0c5c1d054fb
TheGod7 <br.iancefi12@gmail.com>
2025-12-24 16:05:38 -0600

    chore(media): add media constants for file upload configuration

---
+++ backend/src/infra/media/media.contants.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_UPLOAD_MAX_FILE_SIZE_MB = '20';
+export const DEFAULT_UPLOAD_MAX_FILES = '50';
+export const DEFAULT_UPLOAD_MAX_FIELD_SIZE_MB = '5';
+export const DEFAULT_UPLOAD_MAX_PARTS = '60';
+
+export const ALLOWED_IMAGE_MIMETYPES = 'image/jpeg,image/png,image/webp';
+export const ALLOWED_ARCHIVE_MIMETYPES = 'application/zip';

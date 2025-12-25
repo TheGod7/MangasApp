8e866113122284109bbeed90b89768ae63a93f0e
TheGod7 <br.iancefi12@gmail.com>
2025-12-24 19:44:13 -0600

    chore(media): add ConfigModule import for Cloudinary configuration

--- backend/src/infra/media/cloudinary/cloudinary.module.ts
+++ backend/src/infra/media/cloudinary/cloudinary.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
 import { CloudinaryProvider } from './cloudinary.provider';
+import { ConfigModule } from '@nestjs/config';
+import { cloudinaryConfig } from './cloudinary.config';
 
 @Module({
+  imports: [ConfigModule.forFeature(cloudinaryConfig)],
   providers: [CloudinaryProvider, CloudinaryService],
   exports: [CloudinaryService],
 })

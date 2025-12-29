48ad4b5888b112d391c36e14c7d28a24b668125e
TheGod7 <br.iancefi12@gmail.com>
2025-12-29 02:18:38 -0600

    feat(media): standardize getPublicUrlsMany to return GetUrlsManyResult

--- backend/src/infra/media/types/media.interface.ts
+++ backend/src/infra/media/types/media.interface.ts
@@ -17,6 +17,16 @@ export interface DeleteFailure {
   error: string;
 }
 
+export interface GetUrlSuccess {
+  publicId: string;
+  url: string;
+}
+
+export interface GetUrlFailure {
+  publicId: string;
+  error: string;
+}
+
 export interface UploadManyResult {
   successes: UploadSuccess[];
   failures: UploadFailure[];
@@ -27,11 +37,17 @@ export interface DeleteManyResult {
   failures: DeleteFailure[];
 }
 
+export interface GetUrlsManyResult {
+  successes: GetUrlSuccess[];
+  failures: GetUrlFailure[];
+}
+
 export abstract class MediaService {
   abstract upload(file: Express.Multer.File): Promise<string>;
   abstract delete(publicId: string): Promise<void>;
   abstract getPublicUrl(publicId: string): Promise<string>;
-  abstract getPublicUrlsMany(publicIds: string[]): Promise<string[]>;
+
+  abstract getPublicUrlsMany(publicIds: string[]): Promise<GetUrlsManyResult>;
 
   abstract uploadMany(files: Express.Multer.File[]): Promise<UploadManyResult>;
   abstract deleteMany(publicIds: string[]): Promise<DeleteManyResult>;

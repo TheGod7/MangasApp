cd14529705c331556af0d6d1ab44990efcf8e6ec
TheGod7 <br.iancefi12@gmail.com>
2025-12-29 16:43:23 -0600

    test(cloudinary): extract mock constants to dedicated file

---
+++ backend/src/infra/media/providers/cloudinary/__test__/mocks/cloudinary.mock.constants.ts
@@ -0,0 +1,14 @@
+const DEFOULT_PUBLICID = 'test-id-';
+const URL_PREFIX = 'https://example.com/';
+const JPG_EXT = '.jpg';
+
+const CLOUDINARY_OK = 'ok';
+const CLOUDINARY_DELETED = 'deleted';
+
+export {
+  DEFOULT_PUBLICID,
+  URL_PREFIX,
+  JPG_EXT,
+  CLOUDINARY_OK,
+  CLOUDINARY_DELETED,
+};

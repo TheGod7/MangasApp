b4849fc62701650f27d9912e0cd522daaa4ab378
TheGod7 <br.iancefi12@gmail.com>
2025-12-31 20:01:49 -0600

    refactor(validation): replace class-validator with nestjs-zod

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,31 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ZodValidationPipe, cleanupOpenApiDoc } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v1');
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  app.useGlobalPipes(new ZodValidationPipe());
 
   app.enableCors();
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('MangasApp API')
     .setDescription('A simple API for a manga app scanlation')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api', app, document);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('/api', app, cleanupOpenApiDoc(document));
 
   await app.listen(process.env.PORT ?? 3000);
 }
